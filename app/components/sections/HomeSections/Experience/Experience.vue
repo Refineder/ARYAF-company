bf07006e3c0d77160b7934b30d3f52b6abd4f8d4
Yousefharara <hrarty37@gmail.com>
2026-08-29 16:51:37 +0300

    - style: Update Experience style #99 done

--- app/components/sections/HomeSections/Experience/Experience.vue
+++ app/components/sections/HomeSections/Experience/Experience.vue
@@ -14,41 +14,43 @@ const scroll = (direction: 'next' | 'prev') => {
 </script>
 
 <template>
-  <section class="section-container flex flex-col gap-6 py-10 bg-white">
-    <article class="flex flex-wrap items-center justify-between gap-4">
-      <h2 class="font-semibold clmap-text-md">
-        انطلق بتجارتك<span class="gradient-text">،مهما كان قطاعك</span>
-      </h2>
-      <div class="flex gap-2 items-center">
-        <div
-          class="p-3 rounded-full bg-muted cursor-pointer hover:bg-accent"
-          @click="scroll('next')"
-        >
-          <ArrowRight stroke-width="1" />
+  <div class="py-10 bg-white">
+    <section class="section-container flex flex-col gap-6">
+      <article class="flex flex-wrap items-center justify-between gap-4">
+        <h2 class="font-semibold clamp-text-md">
+          انطلق بتجارتك<span class="gradient-text">،مهما كان قطاعك</span>
+        </h2>
+        <div class="flex gap-2 items-center">
+          <div
+            class="p-3 rounded-full bg-muted cursor-pointer hover:bg-accent"
+            @click="scroll('next')"
+          >
+            <ArrowRight stroke-width="1" />
+          </div>
+          <div
+            class="p-3 rounded-full bg-muted cursor-pointer hover:bg-accent"
+            @click="scroll('prev')"
+          >
+            <ArrowLeft stroke-width="1" />
+          </div>
         </div>
-        <div
-          class="p-3 rounded-full bg-muted cursor-pointer hover:bg-accent"
-          @click="scroll('prev')"
-        >
-          <ArrowLeft stroke-width="1" />
-        </div>
-      </div>
-    </article>
+      </article>
 
-    <article
-      ref="sliderRef"
-      class="flex gap-2 items-center overflow-x-auto scroll-smooth snap-x scrollbar-hide"
-    >
-      <div
-        v-for="data in EXPERIENCE_DATA"
-        :key="data.title"
-        class="snap-start shrink-0 w-55 p-4 rounded-lg border-[3px] border-transparent bg-background cursor-pointer hover:gradient-border-bg flex flex-col justify-center items-center gap-2"
+      <article
+        ref="sliderRef"
+        class="flex gap-2 items-center overflow-x-auto scroll-smooth snap-x scrollbar-hide"
       >
-        <div>
-          <img :src="data.thumb" :alt="data.title" />
+        <div
+          v-for="data in EXPERIENCE_DATA"
+          :key="data.title"
+          class="snap-start shrink-0 w-55 p-4 rounded-lg border-[3px] border-transparent bg-background cursor-pointer hover:gradient-border-bg flex flex-col justify-center items-center gap-2"
+        >
+          <div>
+            <img :src="data.thumb" :alt="data.title" />
+          </div>
+          <p class="text-lg">{{ data.title }}</p>
         </div>
-        <p class="text-lg">{{ data.title }}</p>
-      </div>
-    </article>
-  </section>
+      </article>
+    </section>
+  </div>
 </template>
